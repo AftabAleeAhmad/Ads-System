bd4f8bae7f43392b39634e40178a9fdd9fe93329
Aftab Ahmad <138659210+AftabAleeAhmad@users.noreply.github.com>
2023-08-30 19:48:46 +0500

    Update admin.py

--- ads_system/Ads_app/admin.py
+++ ads_system/Ads_app/admin.py
@@ -1,11 +1,10 @@
 from django.contrib import admin
-from .models import Location, Ad , VisitorCount , DailyLocationReport
+from .models import Location, Ad ,, DailyLocationReport
 
 # Register your models here.
 
 admin.site.register(Location)
 admin.site.register(Ad)
-admin.site.register(VisitorCount)
 admin.site.register(DailyLocationReport)
 
 
